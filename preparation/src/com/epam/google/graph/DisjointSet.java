a8bfd18329a4a0caebe26b19976b9ebce96b4690
polashi_bordoloi <polashibordoloi@gmail.com>
2025-01-08 15:20:19 +0530

    Graph problems- Striver sheet

---
+++ preparation/src/com/epam/google/graph/DisjointSet.java
@@ -0,0 +1,66 @@
+package com.epam.google.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DisjointSet {
+    int vertices;
+
+    List<Integer> parents = new ArrayList<>();
+    List<Integer> ranks = new ArrayList<>();
+    List<Integer> sizes = new ArrayList<>();
+
+    public DisjointSet(int V){
+        vertices = V;
+        for(int i=0; i<V; i++){
+            ranks.add(0);
+            parents.set(i,i);
+        }
+    }
+
+    public int findUltimateParent(int node){
+        if(parents.get(node) == node){
+            return node;
+        }
+        int ulp = findUltimateParent(parents.get(node));
+        parents.set(node,ulp);
+        return parents.get(node);
+    }
+
+    public void unionByRank(int u, int v){
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(ranks.get(ulp_u) < ranks.get(ulp_v)){
+            parents.set(ulp_u, ulp_v);
+        } else if (ranks.get(ulp_u) > ranks.get(ulp_v)) {
+            parents.set(ulp_v,ulp_u);
+        }else{
+            parents.set(ulp_v, ulp_u);
+            int rankU = ranks.get(ulp_u);
+            ranks.set(ulp_u, rankU+1);
+        }
+    }
+    public void unionBySize(int u, int v){
+        int ulp_u = findUltimateParent(u);
+        int ulp_v = findUltimateParent(v);
+        if(ulp_u == ulp_v){
+            return;
+        }
+        if(sizes.get(ulp_u) < sizes.get(ulp_v)){
+            sizes.set(ulp_u, ulp_v);
+        }else if(sizes.get(ulp_u) > sizes.get(ulp_v)){
+            sizes.set(ulp_v, ulp_u);
+        }else{
+            parents.set(ulp_u, ulp_v);
+            int size = sizes.get(ulp_v);
+            sizes.set(ulp_v, size+1);
+        }
+
+    }
+    public void pathCompression(){
+
+    }
+}
